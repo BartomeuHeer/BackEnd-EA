24d72e8fc3e9a2a80d5d9c3f29dc107b9aebba69
Bartomeu Heer <bheersalva@gmail.com>
2022-12-11 13:27:31 +0100

    duplicate user

--- src/controller/clientController.ts
+++ src/controller/clientController.ts
@@ -12,12 +12,16 @@ const register = async (req: Request, res: Response) => {
 	let password = req.body.password;
 	const admin = req.body.admin;
 	password = CryptoJS.AES.encrypt(password, 'secret key 123').toString();
+
 	const newUser = new User({ name, email, password, birthday, admin });
-	await newUser.save();
-	res.status(200).json(newUser);
+	await newUser.save().then((data) => {
+		res.status(200).json(data);
+	}).catch((error) => {
+		res.status(404).json("duplicat user");
+	});
 };
 
-const login = async (req: Request, res : Response) => {
+const login = async (req: Request, res: Response) => {
 	const user = await User.findOne({ email: req.body.email });
 	if (!user) {
 		return res.status(404).json({ message: 'User not found' });
@@ -31,12 +35,12 @@ const login = async (req: Request, res : Response) => {
 	const payload = {
 		id: user._id.toString(),
 		isAdmin: user.admin
-	} ;
+	};
 	// let iv = CryptoJS.enc.Hex.parse("FgLFXEr1MZl2mEnk");
 	// secretToken = CryptoJS.AES.encrypt(secretToken,iv).toString();
 	const token = jwt.sign(
-		{id: user._id, isAdmin: user.admin},secretToken,{expiresIn: 3600})
-	res.status(200).json({ user,token });
+		{ id: user._id, isAdmin: user.admin }, secretToken, { expiresIn: 3600 })
+	res.status(200).json({ user, token });
 };
 
 
@@ -46,13 +50,13 @@ const newRating = async (req: Request, res: Response) => {
 	const comment = req.body.comment;
 	const dest = req.body.dest;
 	const rate = req.body.rate;
-	const userA = await User.findOne({name:author});
-	const userD = await User.findOne({name:dest});
-	if (!userA||!userD) {
+	const userA = await User.findOne({ name: author });
+	const userD = await User.findOne({ name: dest });
+	if (!userA || !userD) {
 		return res.status(400).json({ message: 'User not found' });
 	}
 
-	const ratingNew = new Rating({ author:userA.id, comment, dest:userD.id, rate});
+	const ratingNew = new Rating({ author: userA.id, comment, dest: userD.id, rate });
 	await ratingNew.save();
 
 	res.status(200);
@@ -60,7 +64,7 @@ const newRating = async (req: Request, res: Response) => {
 
 const deleteOne = async (req: Request, res: Response) => {
 	const user = await User.findByIdAndDelete(req.params.id);
-	if(!user){
+	if (!user) {
 		return res.status(404).send('No user found')
 	}
 	return res.status(200).json(user);
@@ -82,8 +86,8 @@ const getProfile = async (req: Request, res: Response) => {
 
 const getall = async (req: Request, res: Response) => {
 	// if(req.body.admin === true){
-		const users = await User.find();
-		res.json(users);
+	const users = await User.find();
+	res.json(users);
 	// }
 
 };
@@ -94,19 +98,19 @@ const getone = async (req: Request, res: Response) => {
 };
 
 const getRatings = async (req: Request, res: Response) => {
-	const ratings = await Rating.find({dest:req.params.id}).populate('user');
+	const ratings = await Rating.find({ dest: req.params.id }).populate('user');
 	res.json(ratings);
 };
-const updateUser = async (req: Request, res: Response) =>{
+const updateUser = async (req: Request, res: Response) => {
 	const user = await User.findById(req.params.id);
 	if (!user) {
 		return res.status(404).send('No user found.');
 	}
-	else{
+	else {
 		const password = CryptoJS.AES.encrypt(req.body.password, 'secret key 123').toString();
-		const update= {name: req.body.name,password,email: req.body.email, birthday: req.body.birthday}
-		const user1 = await User.findOneAndUpdate({id:req.params.id}, update);
-		res.json ({status: 'User updated' , user1})
+		const update = { name: req.body.name, password, email: req.body.email, birthday: req.body.birthday }
+		const user1 = await User.findOneAndUpdate({ id: req.params.id }, update);
+		res.json({ status: 'User updated', user1 })
 	}
 }
 const changePass = async (req: Request, res: Response) => {
@@ -114,14 +118,14 @@ const changePass = async (req: Request, res: Response) => {
 	if (!user) {
 		return res.status(404).send('No user found.');
 	}
-	if(req.body.password === CryptoJS.AES.decrypt(user.password!, 'secret key 123').toString(CryptoJS.enc.Utf8)){
+	if (req.body.password === CryptoJS.AES.decrypt(user.password!, 'secret key 123').toString(CryptoJS.enc.Utf8)) {
 		let newpassword = req.body.newpassword;
 		newpassword = CryptoJS.AES.encrypt(newpassword, 'secret key 123').toString();
 		user.password = newpassword;
 		await user.save();
 		res.json({ status: 'User Updated' });
 	}
-	else{
+	else {
 		res.json({ status: 'Wrong password' });
 	}
 };
